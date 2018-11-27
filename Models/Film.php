b7dfe25df769ebce79fa447233db8221e8943b13
Lucas <lucas180395@gmail.com>
2018-11-27 17:00:44 +0100

    htacess tenté mais ne marche pas

--- Models/Film.php
+++ Models/Film.php
@@ -9,7 +9,7 @@ $dbh = new PDO('mysql:host='. $host .';dbname='. $dbname, $user, $pass);
 function getAllMovies() {
     global $dbh;
 
-    $movies = $dbh->query('SELECT * FROM movies');
+    $movies = $dbh->query('SELECT * FROM films');
 
     return $movies->fetchAll();
 
@@ -18,7 +18,7 @@ function getAllMovies() {
 function getOneMovie($id) {
     global $dbh;
 
-    $movies = $dbh->query('SELECT * FROM movies WHERE id='.$id.';');
+    $movies = $dbh->query('SELECT * FROM films WHERE id='.$id.';');
 
     return $movies->fetch();
 }
@@ -27,7 +27,7 @@ function getMoviesByGenre($gender_id) {
     
     global $dbh;
 
-    $movies = $dbh->query('SELECT * FROM movies WHERE gender_id='.$gender_id.';');
+    $movies = $dbh->query('SELECT * FROM films WHERE genre='.$gender_id.';');
 
     return $movies->fetchAll();
 }
